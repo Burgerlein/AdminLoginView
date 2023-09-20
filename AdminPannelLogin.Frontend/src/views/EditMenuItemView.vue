237b6b13847a8255f8e0c72f543ef3664cb1454b
Tim <tim@tim.tim>
2023-09-20 10:34:50 +0200

    create edit Page with design

--- AdminPannelLogin.Frontend/src/views/EditMenuItemView.vue
+++ AdminPannelLogin.Frontend/src/views/EditMenuItemView.vue
@@ -1,5 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  id: Number
+});
+
+import { ref } from 'vue';
+import type { MenuItem } from '@/api/MenuItem';
+import api from '@/api';
+
+const menuItem = ref<MenuItem | undefined>();
+api.fetchMenuItemById(props.id!).then((result) => (menuItem.value = result));
+</script>
 <template>
-  <div></div>
+  <div v-if="menuItem">
+    <form action="">
+      <p>Deutscher Item Name:</p>
+      <input :value="menuItem?.nameDe" type="text" />
+      <p>Englischer Item Name:</p>
+      <input :value="menuItem?.nameEn" type="text" />
+      <p>Deutsche Beschreibung:</p>
+      <input :value="menuItem?.descriptionDe" type="text" />
+      <p>Englische Beschreibung:</p>
+      <input :value="menuItem?.descriptionEn" type="text" />
+      <p>Preis:</p>
+      <input :value="menuItem?.price" type="text" />
+      <br />
+      <input type="submit" />
+    </form>
+  </div>
+  <div v-else>
+    <h1>could not fetch the data</h1>
+  </div>
 </template>
-<style scoped></style>
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+p {
+  font-size: 0.8vw;
+  margin-top: 10px;
+}
+input[type='text'] {
+  background-color: #393c40;
+  color: white;
+  border: #545c66;
+  padding: 6px;
+  font-size: 1vw;
+  width: 100%;
+  text-align: center;
+}
+
+@media (max-width: 1024px) {
+  p {
+    font-size: 2.5vw;
+    margin-top: 10px;
+  }
+  input[type='text'] {
+    font-size: 3vw;
+    width: 100%;
+  }
+}
+</style>
