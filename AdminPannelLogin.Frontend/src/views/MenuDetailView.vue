094ee87c3f4b061976b8b1d56023b00bbebbe1d0
Tim <tim@tim.tim>
2023-09-20 09:28:49 +0200

    Design MenuItemView Page

--- AdminPannelLogin.Frontend/src/views/MenuDetailView.vue
+++ AdminPannelLogin.Frontend/src/views/MenuDetailView.vue
@@ -1,11 +1,43 @@
 <script lang="ts" setup>
-defineProps({
+const props = defineProps({
   id: Number
 });
+
+import { ref } from 'vue';
+import type { MenuItem } from '@/api/MenuItem';
+import api from '@/api';
+
+const menuItem = ref<MenuItem | undefined>();
+api.fetchMenuItemById(props.id!).then((result) => (menuItem.value = result));
 </script>
 
 <template>
-  <div>ID: {{ id }}</div>
+  <div v-if="menuItem" class="menuItemDetailViewContainer">
+    <div>
+      <h1>{{ menuItem?.nameDe }} / {{ menuItem?.nameEn }}</h1>
+      <h3>{{ menuItem?.descriptionDe }}</h3>
+      <h3>{{ menuItem?.descriptionEn }}</h3>
+    </div>
+    <div>
+      <h2>{{ menuItem?.price }}</h2>
+    </div>
+
+    <div>
+      <button>Edit BTN</button>
+    </div>
+  </div>
+
+  <div v-else>
+    <h1>could not fetch the data</h1>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  color: white;
+}
+.menuItemDetailViewContainer {
+  display: grid;
+  grid-template-columns: auto auto;
+}
+</style>
